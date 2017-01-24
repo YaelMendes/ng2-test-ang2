21055f2973becd71337eecd31752eda1b84b1c66
YaelMendes <yael.mendes@gmail.com>
2017-01-24 14:09:33 +0100

    exemple avec obs plutot que promise

--- ProjectTest/src/app/app.module.ts
+++ ProjectTest/src/app/app.module.ts
@@ -4,17 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {CommentListComponent} from "./test-one-story.component";
+import {DataService} from "./data.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CommentListComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [ DataService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
